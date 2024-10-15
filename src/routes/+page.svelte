e1c2051993eb433c9dd8635728ec2c402ef4c29a
jaysprograms <jason.piyasron@gmx.net>
2024-10-15 15:10:47 +0200

    Added more triangles and changed rotation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,26 @@
 <div class="relative overflow-hidden">
-    <div class="background-image tri1">
-        <img src="/tri_happy.png" alt="happy triangle" />
+  <img class="background-image tri1" src="/tri_happy.png" alt="happy triangle" />
+  <img class="background-image tri2" src="/tri_happy.png" alt="happy triangle" />
+  <img class="background-image tri3" src="/tri_happy.png" alt="happy triangle" />
+  <img class="background-image tri4" src="/tri_unhappy.png" alt="unhappy triangle" />
+  <img class="background-image tri5" src="/tri_unhappy.png" alt="unhappy triangle" />
+  <img class="background-image tri6" src="/tri_unhappy.png" alt="unhappy triangle" />
+  <img class="background-image tri7" src="/tri_mad.png" alt="mad triangle" />
+  <img class="background-image tri8" src="/tri_mad.png" alt="mad triangle" />
+  <img class="background-image tri9" src="/tri_mad.png" alt="mad triangle" />
+  <div class="w-screen h-screen grid grid-rows-4 p-12 bg-dark-olive">
+      <div class="row-span-2 flex justify-center items-center">
+          <img src="/Erde.png" alt="earth" class="w-56 z-10"/>
       </div>
-      <div class="background-image tri2">
-        <img src="/tri_unhappy.png" alt="unhappy triangle" />
+      <div class="row-span-2 flex flex-col justify-center items-start text-left z-10">
+          <h1 class="text-4xl font-bold text-light-olive mb-4">CO<sub>2</sub>ntrol</h1>
+          <p class="font-bold text-light-olive mb-6">Time for change</p>
+          <div class="flex flex-col space-y-4 w-full">
+              <button class="w-full font-bold py-2 px-4 rounded bg-olive text-light-olive" on:click={() => {window.location.href = '/auth';}}>Login</button>
+              <button class="w-full font-bold py-2 px-4 rounded bg-light-olive text-black" on:click={() => {window.location.href = '/auth';}}>Register</button>
+          </div>        
       </div>
-      <div class="background-image tri3">
-        <img src="/tri_mad.png" alt="happy triangle" />
-      </div>
-    <div class="w-screen h-screen grid grid-rows-4 p-12 bg-dark-olive">
-        <div class="row-span-2 flex justify-center items-center">
-            <img src="/Erde.png" alt="earth" class="w-56"/>
-        </div>
-        <div class="row-span-2 flex flex-col justify-center items-start text-left">
-            <h1 class="text-4xl font-bold text-light-olive mb-4">CO<sub>2</sub>ntrol</h1>
-            <p class="font-bold text-light-olive mb-6">Time for change</p>
-            <div class="flex flex-col space-y-4 w-full">
-                <button class="w-full font-bold py-2 px-4 rounded bg-olive text-light-olive" on:click={() => {window.location.href = '/auth';}}>Login</button>
-                <button class="w-full font-bold py-2 px-4 rounded bg-light-olive text-black" on:click={() => {window.location.href = '/auth';}}>Register</button>
-            </div>        
-        </div>
-    </div>
+  </div>
 </div>
 
 <style>
@@ -36,27 +36,63 @@
   
     .background-image {
       position: absolute;
-      width: 10rem; 
-      height: 3rem;
+      width: 5rem; 
+      height: 5rem;
       opacity: 1;
       z-index: 1; 
     }
   
     .tri1 {
-      top: 10%;
-      left: 10%;
+      top: 1%;
+      left: 5%;
       animation: rotate 8s linear infinite;
     }
   
     .tri2 {
-      top: 50%;
-      left: 40%;
+      top: 5%;
+      left: 70%;
       animation: rotate 12s linear infinite;
     }
   
     .tri3 {
+      top: 20%;
+      left: 10%;
+      animation: rotate 6s linear infinite;
+    }
+
+    .tri4 {
+      top: 50%;
+      left: 10%;
+      animation: rotate 8s linear infinite;
+    }
+
+    .tri5 {
+      top: 40%;
+      left: 50%;
+      animation: rotate 12s linear infinite;
+    }
+
+    .tri6 {
+      top: 60%;
+      left: 70%;
+      animation: rotate 16s linear infinite;
+    }
+
+    .tri7 {
+      top: 80%;
+      left: 70%;
+      animation: rotate 12s linear infinite;
+    }
+
+    .tri8 {
       top: 30%;
-      left: 60%;
+      left: 70%;
+      animation: rotate 8s linear infinite;
+    }
+
+    .tri9 {
+      top: 90%;
+      left: 10%;
       animation: rotate 6s linear infinite;
     }
   </style>

414990e6a5421427db95020f16b85bbb8302f319
paulpaulsen55 <paulpaulsenx@gmail.com>
2024-11-21 23:41:35 +0100

    remove unused temp.ts

--- src/lib/temp.ts
+++
@@ -1,9 +0,0 @@
-export function enhance(form: HTMLFormElement) {
-    () => {
-        let loading = true;
-        return async ({ update }) => {
-            loading = false;
-            update();
-        };
-    }
-}
